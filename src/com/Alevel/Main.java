ef07939fba70f6762b0248d679ff669cc40bd518
DESKTOP-TI0P950\alevel <aleve@gmail.com>
2021-07-29 18:56:07 +0300

    task

--- src/com/Alevel/Main.java
+++ src/com/Alevel/Main.java
@@ -9,18 +9,20 @@ class Packer {
         Box<Food> foodBox = new Box<>();
         Box<Cake> cakeBox = new Box<>();
         Box<Tart> tartBox = new Box<>();
+        Box<Bakery> bakeryBox = new Box<>();
 
         foodBox.put(new Food());
         cakeBox.put(new Cake());
         tartBox.put(new Tart());
+        bakeryBox.put(new Bakery());
 
         repackage(cakeBox, foodBox);
-        repackage(foodBox, cakeBox);
         repackage(foodBox, foodBox);
-        repackage(foodBox, tartBox);
+        repackage(tartBox, foodBox);
+        repackage(bakeryBox, foodBox);
     }
 
-    public static void repackage(Box<? super Bakery> src, Box<? extends Goods> dest) {
+    public static void repackage(Box<? extends Food> src, Box<? super Food> dest) {
         dest.put(src.get());
     }
 }
