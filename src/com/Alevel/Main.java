36eba7b77188d973f58773e359db2ec77bfde384
valera2407 <valerafatkulin7@gmail.com>
2021-07-30 11:10:54 +0300

    1

--- src/com/Alevel/Main.java
+++ src/com/Alevel/Main.java
@@ -1,28 +1,10 @@
 package com.Alevel;
 
-import com.Alevel.fd.*;
 
 class Packer {
 
     public static void main(String[] args) {
 
-        Box<Food> foodBox = new Box<>();
-        Box<Cake> cakeBox = new Box<>();
-        Box<Tart> tartBox = new Box<>();
-        Box<Bakery> bakeryBox = new Box<>();
-
-        foodBox.put(new Food());
-        cakeBox.put(new Cake());
-        tartBox.put(new Tart());
-        bakeryBox.put(new Bakery());
-
-        repackage(cakeBox, foodBox);
-        repackage(foodBox, foodBox);
-        repackage(tartBox, foodBox);
-        repackage(bakeryBox, foodBox);
-    }
-
-    public static void repackage(Box<? extends Food> src, Box<? super Food> dest) {
-        dest.put(src.get());
     }
 }
+
